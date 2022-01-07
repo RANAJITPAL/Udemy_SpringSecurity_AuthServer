d2c92bd9f9a59a31bb34838cd1b28fea307e26ec
RANAJITPAL <palranajit98@gmail.com>
2022-01-07 12:46:18 +0530

    Initial commit Auth Server

---
+++ src/test/java/com/ranajit/security/authserver/AuthserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ranajit.security.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
